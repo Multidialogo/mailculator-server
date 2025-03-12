44e38a5202c86ba06c3a63585d88a53bb7e49908
Simone Salerni <salerni@netbuilder.it>
2025-03-12 12:18:22 +0100

    having local env with docker compose

--- local/test.sh
+++ local/test.sh
@@ -2,6 +2,4 @@
 
 script_dir=$(dirname $(realpath -s $0))
 
-docker compose -f "$script_dir/docker/compose.yml" exec app go mod tidy
 docker compose -f "$script_dir/docker/compose.yml" exec app go test ./...
-chown 1000:1000 -R "$script_dir/.."
