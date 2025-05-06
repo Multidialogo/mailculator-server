cdeee535a3776430290e0e1420a42c8da99849fe
lorenzo.massarini <massarini@multidialogo.it>
2025-05-06 12:23:21 +0200

    aggiunto comando di build e aggiunto script di build, aggiunti inoltre compose file

--- build-ci.sh
+++ build-ci.sh
@@ -5,7 +5,6 @@ IMAGE_NAME="${ENVIRONMENT}-${SERVICE_NAME}"
 build() {
   (
     docker build -t "${IMAGE_NAME}:latest" -f Dockerfile .
-    docker image ls
     docker tag "${IMAGE_NAME}:latest" "${DOCKER_REGISTRY}/${IMAGE_NAME}:latest"
     docker tag "${IMAGE_NAME}:latest" "${DOCKER_REGISTRY}/${IMAGE_NAME}:${GITHUB_SHA}"
     docker push "${DOCKER_REGISTRY}/${IMAGE_NAME}" --all-tags
