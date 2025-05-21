57fe3e6065ea7d9a36cbc87ce9563b9851f24161
lorenzo.massarini <massarini@multidialogo.it>
2025-05-21 17:55:07 +0200

    aggiunto put parameter per nuova task definition

--- cdk/task_definition_stack.py
+++ cdk/task_definition_stack.py
@@ -179,7 +179,7 @@ class TaskDefinitionStack(Stack):
         log_group = logs.LogGroup(
             scope=self,
             id=f'{service_name}-log-group',
-            log_group_name=f'{selected_environment}/{MULTICARRIER_EMAIL_ID}/{service_name}',
+            log_group_name=f'{selected_environment}/{MULTICARRIER_EMAIL_ID}/{service_name}2',
             removal_policy=log_group_retainment,
             retention=logs.RetentionDays.ONE_MONTH
         )
