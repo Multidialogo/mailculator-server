f4d2051704e81d1ead309b8b6596b5c4258bdc40
lorenzo.massarini <massarini@multidialogo.it>
2025-05-21 15:55:20 +0200

    aggiunto put parameter per nuova task definition

---
+++ cdk/get_env_variables.py
@@ -0,0 +1,46 @@
+import os
+
+def check_variable(variable, env_variable_name: str):
+    if not variable:
+        raise Exception(f'{env_variable_name} variable must be set.')
+
+ENVIRONMENT_VARIABLES = [
+    'REGION',
+    'ACCOUNT_ID',
+    'SERVICE_NAME',
+    # 'CLUSTER_NAME',
+    # 'VPC_ID',
+    # 'MD_REST_EFS_ID',
+    'MD_REST_EFS_FOLDER_NAME',
+    # 'MD_REST_EFS_SECURITY_GROUP_ID',
+    'MC_EMAIL_EFS_FOLDER_NAME',
+    # 'MD_DOMAIN',
+    # 'ALB_RULE_PRIORITY',
+    # 'IMAGE_TAG',
+    'SERVICE_CPU',
+    'SERVICE_MEMORY',
+    # 'SERVICE_DESIRED_COUNT',
+    # 'SERVICE_MAX_COUNT',
+    'SERVICE_CONTAINER_PORT',
+    'SERVICE_HOST_PORT',
+    # 'SSM_INTERNAL_ALB_ARN'
+]
+
+class GetEnvVariables:
+    def __init__(
+            self,
+            selected_environment: str
+    ) -> None:
+
+        self.env_dict = {
+            'SELECTED_ENVIRONMENT': selected_environment,
+        }
+
+        for i in ENVIRONMENT_VARIABLES:
+
+            if i == 'IMAGE_TAG':
+                self.env_dict[i] = 'latest'
+                continue
+
+            check_variable(os.environ[i], i)
+            self.env_dict[i] = os.environ[i]
